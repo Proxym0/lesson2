d334861e479b1b98f7bf2a6ae9fd54fe27788ee3
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-23 23:07:47 +0300

    save finish lesson-2.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         for(int i=0;i<10;i++){
             String name=scanner.nextLine();
-            System.out.println("Hello world!");
+            System.out.println("Hello "+name);
         }
     }
 }
